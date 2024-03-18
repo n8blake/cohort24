2411ae87514a6cd8233f37e159c8102ceeb537ba
Nathan Blake <n8blake@mac.com>
2024-03-18 19:47:05 -0400

    Remove files from template project

--- client/src/app/models/astro-sign.model.ts
+++
@@ -1,5 +0,0 @@
-export interface IAstroSign {
-    name: string;
-    startDate: Date;
-    endDate: Date;
-}
--- client/src/app/models/horoscope.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IAstroSign } from "./astro-sign.model";
-
-export interface IHoroscope {
-    date: Date;
-    sign: IAstroSign | string;
-    horoscope: string;
-}
