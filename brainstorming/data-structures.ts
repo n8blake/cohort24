a570202f4a5053f4e15d992cb60b35d32f98f12a
Nathan Blake <n8blake@mac.com>
2024-03-20 22:43:03 -0400

    more brainstorming

--- brainstorming/data-structures.ts
+++ brainstorming/data-structures.ts
@@ -5,13 +5,19 @@
 // What do other students have to say about it?
 // Volunteer to help others on it
 
+
 interface assignment {
   title: String;
   description?: String;
   dueDate: Date;
   course: String;
   courseId: number;
-  estimatedEffortTime: number;
+}
+
+interface assignmentRating {
+  difficulty: number; // int: 0 - 10
+  estiatedEfforTime: number; // stored in seconds
+  user: ObjectId;
 }
 
 interface course {
