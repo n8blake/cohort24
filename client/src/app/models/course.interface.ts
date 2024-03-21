3dc1d65442ebc78478e54dd32b9e1a34e784de83
Nathan Blake <n8blake@mac.com>
2024-03-20 22:45:45 -0400

    Define course interface

---
+++ client/src/app/models/course.interface.ts
@@ -0,0 +1,58 @@
+// export interface ICourse {
+//     id: number,
+//     name: string,
+//     uuid: string,
+//     course_code: string,
+//     enrollment_term_id: number,
+//     friendly_name?: string,
+//     course_color?: string,
+//     calendar: {
+//         ics: string
+//     },
+
+// }
+export interface ICourse {
+    id: number;
+    name: string;
+    account_id: number;
+    uuid: string;
+    start_at: string | null;
+    grading_standard_id: number | null;
+    is_public: boolean;
+    created_at: string;
+    course_code: string;
+    default_view: string;
+    root_account_id: number;
+    enrollment_term_id: number;
+    license: string;
+    grade_passback_setting: string | null;
+    end_at: string | null;
+    public_syllabus: boolean;
+    public_syllabus_to_auth: boolean;
+    storage_quota_mb: number;
+    is_public_to_auth_users: boolean;
+    homeroom_course: boolean;
+    course_color: string | null;
+    friendly_name: string | null;
+    apply_assignment_group_weights: boolean;
+    calendar: {
+      ics: string;
+    };
+    time_zone: string;
+    blueprint: boolean;
+    template: boolean;
+    enrollments: IEnrollment[];
+    hide_final_grades: boolean;
+    workflow_state: string;
+    restrict_enrollments_to_course_dates: boolean;
+  }
+  
+export interface IEnrollment {
+    type: string;
+    role: string;
+    role_id: number;
+    user_id: number;
+    enrollment_state: string;
+    limit_privileges_to_course_section: boolean;
+  }
+  
